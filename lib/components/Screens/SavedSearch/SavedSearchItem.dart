e3819bf930025e81e423306e72d2fa434e5583ef
himanshighinaiya <himanshighinaiya181@gmail.com>
2020-10-27 15:29:52 +0530

    pull to refress issue resolved in saved Search list

--- lib/components/Screens/SavedSearch/SavedSearchItem.dart
+++ lib/components/Screens/SavedSearch/SavedSearchItem.dart
@@ -32,51 +32,7 @@ class _SavedSearchItemWidgetState extends State<SavedSearchItemWidget>
   @override
   void initState() {
     super.initState();
-    if (widget.searchType == SavedSearchType.savedSearch) {
-      savedSearchBaseList = BaseList(BaseListState(
-//      imagePath: noRideHistoryFound,
-        noDataMsg: APPNAME,
-        noDataDesc: R.string().noDataStrings.noDataFound,
-        refreshBtn: R.string().commonString.refresh,
-        enablePullDown: true,
-        enablePullUp: true,
-        onPullToRefress: () {
-          callApi(true);
-        },
-        onRefress: () {
-          callApi(true);
-        },
-        onLoadMore: () {
-          callApi(false, isLoading: true);
-        },
-      ));
-
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        callApi(false);
-      });
-    } else {
-      recentSearchList = BaseList(BaseListState(
-//      imagePath: noRideHistoryFound,
-        noDataMsg: APPNAME,
-        noDataDesc: R.string().noDataStrings.noDataFound,
-        refreshBtn: R.string().commonString.refresh,
-        enablePullDown: true,
-        enablePullUp: true,
-        onPullToRefress: () {
-          callApi(true);
-        },
-        onRefress: () {
-          callApi(true);
-        },
-        onLoadMore: () {
-          callApi(false, isLoading: true);
-        },
-      ));
-
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        callApi(false);
-      });
-    }
+    callBaseList();
   }
 
   callApi(bool isRefress, {bool isLoading = false}) {
@@ -834,8 +790,7 @@ class _SavedSearchItemWidgetState extends State<SavedSearchItemWidget>
     // setState(() {});
   }
 
-  @override
-  Widget build(BuildContext context) {
+  callBaseList(){
     if (widget.searchType == SavedSearchType.savedSearch) {
       savedSearchBaseList = BaseList(BaseListState(
 //      imagePath: noRideHistoryFound,
@@ -873,6 +828,11 @@ class _SavedSearchItemWidgetState extends State<SavedSearchItemWidget>
         },
       ));
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    callBaseList();
     return widget.searchType == SavedSearchType.savedSearch
         ? savedSearchBaseList ?? SizedBox()
         : recentSearchList ?? SizedBox();
